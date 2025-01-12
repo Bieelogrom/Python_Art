22f12290749a1bc644291fa0e302cc9a3a9d08cb
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-12 00:50:46 -0300

    feat: criado classe usuário

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'id', 'email', 'username', 'bio', 'created_at', 'is_staff'
+    )
