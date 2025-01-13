7079ad40c5afd6edcd203764ba19aab1c2781922
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-13 14:22:45 -0300

    feat: Postagens

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Posts
+
+admin.site.register(Posts)
 
 # Register your models here.
 
