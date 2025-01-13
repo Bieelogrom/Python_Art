42803fb5aa4a00504503d65f3b2150a3a73ec67a
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-12 21:10:56 -0300

    feat: desenvolvendo registro

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_page, name="home_page"),
     path('pagina_de_acesso/', views.pagina_de_acesso, name="pagina_de_acesso"),
     path('posts/', views.posts, name="posts"),
+    path('deslogar/', views.deslogar, name="deslogar"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -10,14 +10,22 @@ def home_page(request):
 
 def pagina_de_acesso(request):
     if request.method == "POST":
-        username = request.POST['input_email']
-        password = request.POST['input_senha']
-        # return HttpResponse(f"<p>{email} e {password}</p>")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('posts', {'user': user})
+        if 'login' in request.POST:
+            username = request.POST['input_email']
+            password = request.POST['input_senha']
+            # return HttpResponse(f"<p>{email} e {password}</p>")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('posts')
+        elif 'cadastro' in request.POST:
+            print("oi")
     return render(request, 'blog/pagina_de_acesso.html', {})
+    
+def deslogar(request):
+    logout(request)
+    return redirect('home_page')
+        
     
 def posts(request):
     return render(request, 'blog/posts.html', {})
