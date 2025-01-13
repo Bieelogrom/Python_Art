81597f9b5942021eaf9fce91955adb7bfbe74669
Bieelogrom <gabrielamorim15436@gmail.com>
2025-01-13 13:45:34 -0300

    feat: criando classe Posts

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, null=False)
     
+class Posts(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=100)
+    imagem = models.ImageField(upload_to='img/posts')
+    autor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    
